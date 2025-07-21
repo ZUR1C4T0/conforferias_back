f5adbc12b662bf158ef629d0675e3853f1ae8f34
ZUR1C4T0 <ivanulloque19@gmail.com>
2025-07-21 16:56:02 -0500

    fix(pm2): Correct script path in ecosystem config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'conforferias_back',
-      script: 'dist/main',
+      script: 'dist/main.js',
       env_production: {
         NODE_ENV: 'production',
       },
